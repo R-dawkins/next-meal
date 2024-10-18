10d135643b322cd74c153f43092b3e045d030873
YGS <tig03027@naver.com>
2024-10-18 15:55:20 +0900

    day 7

--- foodies/app/meals/share/page.jsx
+++ foodies/app/meals/share/page.jsx
@@ -1,13 +1,14 @@
-'use client'
-
-import ImagePicker from '@/components/meals/image-picker';
-import classes from './page.module.css';
-import { shareMeal } from '@/lib/action';
+"use client";
+import { useFormState } from "react-dom";
+import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+import { shareMeal } from "@/lib/action";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
-// use server를 함수 내에서만 사용한다해도
-// use client와 use server를 한 파일 안에서 같이 쓸수는 없다
-
+  // use server를 함수 내에서만 사용한다해도
+  // use client와 use server를 한 파일 안에서 같이 쓸수는 없다
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -45,9 +46,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Your image" name="image"/>
+          <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
