5d2b44c228b717eb9c1bc8ac8c1dcad7843d1575
YGS <tig03027@naver.com>
2024-10-26 00:01:10 +0900

    News day 3

--- news/app/news/[slug]/page.js
+++ news/app/news/[slug]/page.js
@@ -1,5 +1,6 @@
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from 'next/navigation';
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
@@ -11,9 +12,11 @@ export default function NewsDetailPage({ params }) {
   return (
     <article>
       <header className="news-article">
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link scroll={false} href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
-        <time datetime={newsItem.date}>{newsItem.date}</time>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
       <p>{newsItem.content}</p>
     </article>
--- news/components/news-list.jsx
+++ news/components/news-list.jsx
@@ -5,7 +5,7 @@ export default function NewsList({ news }) {
     <ul className="news-list">
       {news.map((newsItem) => (
         <li key={newsItem.id}>
-          <Link href={`/news/${newsItem.slug}`}>
+          <Link scroll={false} href={`/news/${newsItem.slug}`}>
             <img
               src={`/images/news/${newsItem.image}`}
               alt={newsItem.title}
